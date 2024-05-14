d29c017cbc06f9ee7be42af1a2ccc7027e1d6872
Afeef K K <45afeef@gmail.com>
2024-05-14 18:53:04 +0530

    Added topic as chips in lesson_card

--- lib/src/features/lesson/presentation/widgets/lesson_card.dart
+++ lib/src/features/lesson/presentation/widgets/lesson_card.dart
@@ -27,31 +27,65 @@ class LessonCard extends StatelessWidget {
             Text(lesson.value.description,
                 style: AppTextStyle.contentStyleNormal),
             const Gap(),
+            SizedBox(
+              height: 50,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  if (lesson.value.topics != null)
+                    ...lesson.value.topics!.map((e) => Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 2),
+                          child: Chip(
+                            backgroundColor: Colors.greenAccent[100],
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100),
+                              side: const BorderSide(
+                                  color: Colors.green,
+                                  width: 1), // Black border
+                            ),
+                            label: Text(
+                              e,
+                              style: const TextStyle(
+                                  color: Colors.green, fontSize: 10),
+                            ),
+                          ),
+                        )),
+                ],
+              ),
+            ),
+            const Gap(),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 DuolingoIconButton(
+                    borderWidth: 4,
+                    padding: const EdgeInsets.all(18),
                     child: const Icon(
                       Icons.account_tree_rounded,
                       color: Colors.white,
                     ),
                     onPressed: () {
-                      Get.toNamed(AppRoutes.mindMapScreen);
+                      Get.toNamed(AppRoutes.mindMapScreen,
+                          arguments: lesson.value.mindMaps);
                     }),
-                DuolingoIconButton(
-                    color: Colors.red,
-                    shadowColor: Colors.red[900]!,
-                    child: const Icon(
-                      Icons.add_home_work,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {}),
                 DuolingoButton(
+                    padding: const EdgeInsets.all(12),
+                    borderWidth: 4,
+                    elevation: const Offset(4, 8),
                     color: Colors.blue,
-                    shadowColor: Colors.blue[900]!,
                     child: Text(
                       "lbl_assess".tr,
                       style: const TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () {}),
+                DuolingoIconButton(
+                    borderWidth: 4,
+                    padding: const EdgeInsets.all(18),
+                    color: Colors.red,
+                    child: const Icon(
+                      Icons.add_home_work,
+                      color: Colors.white,
                     ),
                     onPressed: () {}),
               ],
